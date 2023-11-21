60ede06f7a94419b4e5b2158a1934724f5a374d2
Jihanestein <jihansbhsbhs@gmail.com>
2023-11-21 13:33:41 -0800

    tasl2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Square module."""
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: length of a side of the square.
+        """
+        self.__size = size
